3f33d47cbcf45f5670a0f7920a345788a7c0a635
jamth4 <105566351+jamth4@users.noreply.github.com>
2023-02-26 14:34:30 -0500

    Create gpiotest.py

---
+++ testing/gpiotest.py
@@ -0,0 +1,32 @@
+import machine
+import utime
+import builtins
+
+number = 0
+done = False
+conTest = ""
+tested = []
+failed = []
+
+while done == False:
+    number = int(input("what pin do you want to check? "))
+    tested.append(number)
+    led = machine.Pin(number, machine.Pin.OUT)
+    led.value(1)
+    utime.sleep(4)
+    led.value(0)
+    passed = input("Did the LED power on? ")
+    passed = passed.lower()
+    if passed == 'no':
+        failed.append(number)
+    conTest = input("Are you done testing pins? ")
+    conTest = conTest.lower()
+    if conTest == 'yes':
+        done = True
+    else:
+        done = False
+print('You tested the following pins:')
+print(tested)
+
+print('The following pins failed:')
+print(failed)
